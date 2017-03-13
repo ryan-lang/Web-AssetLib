b7694063c0dad3e48ca735afda30d51c472dbb40
Ryan Lang <rlang@me.com>
2017-03-13 12:19:38 -0700

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:

--- lib/Web/AssetLib.pm
+++ lib/Web/AssetLib.pm
@@ -1,5 +1,5 @@
 package Web::AssetLib;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Method::Signatures;
 use Moose;
@@ -18,7 +18,7 @@ Web::AssetLib - Moose-based pluggable library manager for compiling and serving 
 
 =head1 VERSION
  
-version 0.03
+version 0.04
 
 =head1 SYNOPSIS
 
